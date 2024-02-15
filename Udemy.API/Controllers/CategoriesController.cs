185105f3ae06b97c76a04bd725a2d2c16f209e2a
Kerem Başçiftçi <kerem.bascifci@gazi.edu.tr>
2024-02-15 23:38:54 +0300

    api için category controller yazıldı -tedium

--- Udemy.API/Controllers/CategoriesController.cs
+++ Udemy.API/Controllers/CategoriesController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Udemy.Core.DTOs;
+using Udemy.Core.Models;
 using Udemy.Core.Services;
 
 namespace Udemy.API.Controllers
@@ -9,10 +12,12 @@ namespace Udemy.API.Controllers
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoriesController(ICategoryService categoryService)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
 
 
@@ -21,6 +26,47 @@ namespace Udemy.API.Controllers
         public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId)
         {
             return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryId));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoryDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categoryDto));
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(200, categoryDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Category categoryDto)
+        {
+            var category = await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+            var categoryyDto = _mapper.Map<CategoryDto>(_mapper.Map<CategoryDto>(category));
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(201, categoryyDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CategoryDto categoryDto)
+        {
+            await _categoryService.UpdateAsync(_mapper.Map<Category>(categoryDto));
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(204));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            await _categoryService.RemoveAsync(category);
+            return CreateActionResult(CustomResponseDto<CategoryDto>.Success(204));
+
         }
 
     }
